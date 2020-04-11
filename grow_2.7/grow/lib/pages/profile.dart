1c784ec73e02fcd65e1b46bd0da15a86092c4958
Omine100 <54552984+Omine100@users.noreply.github.com>
2020-04-11 02:22:14 -0500

    Update profile.dart

--- grow_2.7/grow/lib/pages/profile.dart
+++ grow_2.7/grow/lib/pages/profile.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
 import 'package:grow/services/authentication.dart';
+import 'package:grow/pages/root.dart';
 
 class ProfileScreen extends StatefulWidget {
   ProfileScreen({Key key, this.auth, this.logoutCallback, this.userId});
 
-  //VARIABLE REFERENCE: NEEDED TO LOAD ADD GOAL SCREEN
+  //VARIABLE REFERENCE: NEEDED TO LOAD HOME SCREEN
   final BaseAuth auth;
   final VoidCallback logoutCallback;
   final String userId;
@@ -24,7 +25,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
         children: <Widget>[
           Positioned(
             top: MediaQuery.of(context).size.height * 0.1,
-            left: MediaQuery.of(context).size.width * 0.45,
+            left: MediaQuery.of(context).size.width * 0.4,
             child: Text(
               "PROFILE",
               style: TextStyle(
@@ -35,16 +36,18 @@ class _ProfileScreenState extends State<ProfileScreen> {
           ), //Profile text
           Positioned(
             top: MediaQuery.of(context).size.height * 0.5,
-            left: MediaQuery.of(context).size.width * 0.45,
-            child: GestureDetector(
-              onTap: () {
+            left: MediaQuery.of(context).size.width * 0.375,
+            child: RaisedButton(
+              color: Theme.of(context).accentColor,
+              onPressed: () {
                 widget.auth.signOut();
                 widget.logoutCallback();
               },
               child: Text(
                 "Sign Out",
                 style: TextStyle(
-                  color: Theme.of(context).accentColor,
+                  color: Colors.white,
+                  fontSize: 25.0,
                 ),
               ),
             ),
