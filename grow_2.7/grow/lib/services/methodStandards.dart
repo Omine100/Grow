bce733297081980b2d4c7b030010f0ffa6d3b522
Omine100 <mrb0094@tigermail.auburn.edu>
2020-05-09 15:44:02 -0500

    Update methodStandards.dart

---
+++ grow_2.7/grow/lib/services/methodStandards.dart
@@ -0,0 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+import 'package:grow/services/cloudFirestore.dart';
+
+class MethodStandards {
+  CloudFirestore cloudFirestore = new CloudFirestore();
+
+  //MECHANICS: TIMER
+  void timer(Stopwatch stopwatch, DocumentSnapshot documentSnapshot) {
+    int currentTotal = (stopwatch.elapsedMilliseconds / 1000).toInt();
+    print("Seconds: " + (currentTotal).toString());
+
+    DateTime now  = new DateTime.now();
+    DateTime currentDate = new DateTime(now.year, now.month, now.day);
+    cloudFirestore.updateTimeData(documentSnapshot, currentTotal, currentDate);
+  }
+}
