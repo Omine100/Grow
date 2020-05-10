501c92d408887d056609f5a52a75878d7e5b48bf
Omine100 <54552984+Omine100@users.noreply.github.com>
2020-05-10 12:12:40 -0500

    Update home.dart
    I think I got the favorite timey thing working.

--- grow_2.7/grow/lib/pages/home.dart
+++ grow_2.7/grow/lib/pages/home.dart
@@ -251,16 +251,22 @@ class _HomeScreenState extends State<HomeScreen> {
   //USER INTERFACE: FAVORITE CARD
   Widget buildFavoriteCard(DocumentSnapshot doc) {
     Stopwatch stopwatch = new Stopwatch();
-    db.collection(widget.userId).document(doc['documentId']).snapshots();
-    DocumentReference documentReference = db.collection(widget.userId).document(doc['documentId']);
-    
+    DocumentSnapshot documentSnapshot;
+    var documentReference = db.collection(widget.userId).document(doc['documentId']);
+    documentReference.get().then((DocumentSnapshot) {
+      documentSnapshot = DocumentSnapshot;
+    });
+
     return new GestureDetector( //db.collection(widget.userId).document(documentId).changeCurrentTime
       onTap: () {
         if (stopwatch.isRunning) {
           stopwatch.stop();
           print("Hehe");
-          methodStandards.timer(stopwatch, doc); //Need to figure out how to send the correct document
+          methodStandards.timer(stopwatch, documentSnapshot); //Need to figure out how to send the correct document
           stopwatch.reset();
+          setState(() {
+            print("Set state");
+          });
         } else {
           print("Hoho");
           stopwatch.start();
