6a6c6227a9c65a9575ababfdb5bd2deffbb75ac3
Matthew Browning <54552984+Omine100@users.noreply.github.com>
2020-05-17 20:11:49 -0500

    Update addGoal.dart
    New UI is getting there I think

--- grow_2.7/grow/lib/pages/addGoal.dart
+++ grow_2.7/grow/lib/pages/addGoal.dart
@@ -52,6 +52,7 @@ class _AddGoalScreenState extends State<AddGoalScreen> {
   //USER INTERFACE: SHOW TEXT FIELD FOR TITLE
   Widget showPickTitle() {
     return new Container(
+      height: MediaQuery.of(context).size.height * 0.1,
       width: MediaQuery.of(context).size.width * 0.8,
       child: TextFormField(
         controller: _titleTextEditingController,
@@ -109,6 +110,7 @@ class _AddGoalScreenState extends State<AddGoalScreen> {
                   child: GestureDetector(
                     onTap: () {
                       colorPosition = index;
+                      print("colorPosition: " + colorPosition.toString());
                     },
                     child: Container(
                       height: 50.0,
@@ -148,20 +150,19 @@ class _AddGoalScreenState extends State<AddGoalScreen> {
               itemBuilder: (context, index) {
                 return Padding(
                   padding: EdgeInsets.only(left: 25.0, right: 25.0),
-                  child: GestureDetector(
-                    onTap: () {
+                  child: NeumorphicButton(
+                    onClick: () {
                       iconPosition = index;
+                      print("iconPosition: " + iconPosition.toString());
                     },
-                    child: NeumorphicButton(
-                      boxShape: NeumorphicBoxShape.circle(),
-                      style: NeumorphicStyle(
-                        shape: NeumorphicShape.flat,
-                        depth: 8.0,
-                        lightSource: LightSource.topLeft,
-                        color: Theme.of(context).accentColor,
-                      ),
-                      child: dataLists.getIconData(index),
+                    boxShape: NeumorphicBoxShape.circle(),
+                    style: NeumorphicStyle(
+                      shape: NeumorphicShape.flat,
+                      depth: 8.0,
+                      lightSource: LightSource.topLeft,
+                      color: Theme.of(context).accentColor,
                     ),
+                    child: dataLists.getIconData(index),
                   ),
                 );
               },
@@ -174,74 +175,61 @@ class _AddGoalScreenState extends State<AddGoalScreen> {
 
   //USER INTERFACE: SHOW TIME PICKER FOR TIME
   Widget showPickTime() {
-    return new Container(
-      height: MediaQuery.of(context).size.height * 0.2,
-      child: TimePickerSpinner(
-        normalTextStyle: TextStyle(
-          color: Colors.white,
-          fontSize: 25.0
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.925,
+      child: new InterfaceStandards().parentCenter(context, 
+        TimePickerSpinner(
+          normalTextStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 25.0
+          ),
+          itemHeight: 35.0,
+          highlightedTextStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 35.0
+          ),
+          is24HourMode: true,
+          isForce2Digits: false,
+          isShowSeconds: true,
+          spacing: 25.0,
+          time: DateTime.utc(1,1,1,0,0,0,1),
+          secondsInterval: 1,
+          onTimeChange: (time) {
+            setState(() {
+              dateTime = time;
+            });
+          },
         ),
-        itemHeight: 35.0,
-        itemWidth: 50.0,
-        highlightedTextStyle: TextStyle(
-          color: Colors.white,
-          fontSize: 35.0
-        ),
-        is24HourMode: true,
-        isForce2Digits: false,
-        isShowSeconds: true,
-        spacing: 15.0,
-        time: DateTime.utc(1,1,1,0,0,0,1),
-        secondsInterval: 1,
-        onTimeChange: (time) {
-          setState(() {
-            dateTime = time;
-          });
-        },
       ),
-    );
-  }
-
-  //USER INTERFACE: SHOW FORM
-  Widget showForm() {
-    return Column(
-      children: <Widget>[
-        showPickTitle(),
-        showPickColor(),
-        showPickIcon(),
-        showPickTime(),
-      ],
     );
   }
 
   //USER INTERFACE: SHOW CREATE BUTTON
   Widget showCreateButton() {
     return new InterfaceStandards().parentCenter(context,
-        GestureDetector(
-          onTap: () {
-            cloudFirestore.createData(
-              _titleTextEditingController.text.toString(),
-              iconPosition,
-              colorPosition,
-              goalTimeCalculator(),
-            );
-            Navigator.pop(context);
-          },
-          child: NeumorphicButton(
-            boxShape: NeumorphicBoxShape.circle(),
-            style: NeumorphicStyle(
-              shape: NeumorphicShape.flat,
-              depth: 8.0,
-              lightSource: LightSource.topLeft,
-              color: Theme.of(context).highlightColor,
-            ),
-            child: Icon(
-              Icons.check,
-              color: Theme.of(context).splashColor,
-              size: 35.0,
-            ),
-          ),
+      NeumorphicButton(
+        onClick: () {
+          cloudFirestore.createData(
+            _titleTextEditingController.text.toString(),
+            iconPosition,
+            colorPosition,
+            goalTimeCalculator(),
+          );
+          Navigator.pop(context);
+        },
+        boxShape: NeumorphicBoxShape.circle(),
+        style: NeumorphicStyle(
+          shape: NeumorphicShape.flat,
+          depth: 8.0,
+          lightSource: LightSource.topLeft,
+          color: Theme.of(context).highlightColor,
         ),
+        child: Icon(
+          Icons.check,
+          color: Theme.of(context).splashColor,
+          size: 35.0,
+        ),
+      ),
     );
   }
 
@@ -270,8 +258,22 @@ class _AddGoalScreenState extends State<AddGoalScreen> {
               ),
               Positioned(
                 top: MediaQuery.of(context).size.height * 0.25,
-                left: MediaQuery.of(context).size.width * 0.05,
-                child: showForm()
+                left: MediaQuery.of(context).size.width * 0.1,
+                child: showPickTitle(),
+              ),
+              Positioned(
+                top: MediaQuery.of(context).size.height * 0.35,
+                left: MediaQuery.of(context).size.width * 0.1,
+                child: showPickColor(),
+              ),
+              Positioned(
+                top: MediaQuery.of(context).size.height * 0.475,
+                left: MediaQuery.of(context).size.width * 0.1,
+                child: showPickIcon(),
+              ),
+              Positioned(
+                top: MediaQuery.of(context).size.height * 0.6125,
+                child: showPickTime()
               ),
               Positioned(
                 top: MediaQuery.of(context).size.height * 0.8,
